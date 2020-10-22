5198f43abc1cad98a75cfc2d609936f0d2d714bd
moraagarcia <moragarcia2007@hotmail.com>
2020-10-22 00:53:14 -0300

    actualizo el main

--- main.c
+++ main.c
@@ -1,107 +1,90 @@
 #define _POSIX_C_SOURCE 200112L
 
 #include <string.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include "base_64_encoder.h"
+#include "base_64_decoder.h"
 
 #define SUCCESS 0
 #define ERROR 1
 
 
 int main(int argc, char const *argv[]){ 
-	if (argc > 6 || argc < 2){   //verificar que estén bien estos números
-		perror("Invalid command");
+	if (argc > 6 || argc < 2){
+		fprintf(stderr,"%s \n","Invalid command");
 		return ERROR;
 	}else{
-		//bool error = false;  //ver si sacar después en caso de que no me sirva
 		if (argc == 2){
-			if (argv[1] == "-V"){  //dejo tambien que lo pongan en minúscula?
-				printf("%s", "Versión 1.2"); //qué version habría que poner??
+			if (argv[1] == "-V"){
+				fprintf(stdout,"%s \n", "Version 1.2");
 				return SUCCESS;
 			}
 			if (argv[1] == "-h"){
-				printf("%s", "Usage:");   //Lo imprimo así o todo en una línea con barras n?
-				printf("%s", "tp0 -h");
-				printf("%s", "tp0 -V");
-				printf("%s", "tp0 [options]");
-				printf("%s", "-V, --version Print version and quit.");
-				printf("%s", "-h, --help Print this information.");
-				printf("%s", "-o, --output Path to output file.");
-				printf("%s", "-i, --input Path to input file.");
-				printf("%s", "-d, --decode Decode a base64-encoded file.");
+				fprintf(stdout, "%s \n", "Usage:");
+				fprintf(stdout, "%s \n", "tp0 -h");
+				fprintf(stdout, "%s \n", "tp0 -V");
+				fprintf(stdout, "%s \n", "tp0 [options]");
+				fprintf(stdout, "%s \n", "-V, --version Print version and quit.");
+				fprintf(stdout, "%s \n", "-h, --help Print this information.");
+				fprintf(stdout, "%s \n", "-o, --output Path to output file.");
+				fprintf(stdout, "%s \n", "-i, --input Path to input file.");
+				fprintf(stdout, "%s \n", "-d, --decode Decode a base64-encoded file.");
 				return SUCCESS;
 			}
-			perror("Invalid command");//HAGO UN STDERR??? asi ta bien??
+			fprintf(stderr, "%s \n", "Invalid command");
 			return ERROR;
 		}
 		char* input_file;
 		char* output_file;
 		bool decode = false;
 		bool error = false;
+		int offset = 0;
 		if (argv[1] == "-d"){
 			decode = true;
-			if (argc == 4){
-				input_file = NULL;
-				output_file = argv[3]; 
-			}
-			else{
-				input_file = argv[3];
-				output_file = argv[5];
-			} 
+			offset = 1;
 		}
-		else if (argv[1] == "-i"){
-			input_file = argv[2];
-			output_file = argv[4];
+		if (argv[1 + offset] == "-i"){
+			input_file = argv[2 + offset];
+			output_file = argv[4 + offset];
 		}
-		else if (argv[1] == "-o"){
+		else if (argv[1 + offset] == "-o"){
 			input_file = NULL;
-			output_file = argv[2];
+			output_file = argv[2 + offset];
 		}else{
-			perror("Invalid command");//HAGO UN STDERR??? asi ta bien??
-			return ERROR;  // o puede haber otra alternativa??
+			fprintf(stderr, "%s \n", "Invalid command");
+			return ERROR;
 		}
 		if (decode){
 			base_64_decoder_t decoder;
-			if (base_64_decoder_init(&decoder, input_file)==ERROR){
-				perror("Error initiating decoder");//HAGO UN STDERR??? asi ta bien??
+			if (base_64_decoder_init(&decoder, input_file)==ERROR){  //falta que reciba el output
+				fprintf(stderr, "%s \n", "Error initiating decoder");
 				return ERROR;
 			}
 			if (base_64_decoder_decode(&decoder) == ERROR)
-				bool error = true;
+				error = true;
 			base_64_decoder_destroy(&decoder);
 			if (error){
-				perror("Error decoding");//HAGO UN STDERR??? asi ta bien??
+				fprintf(stderr, "%s \n", "Error decoding");
 				return ERROR;
 			}
 			return SUCCESS;
 		}  
 		else{
-
+			base_64_encoder_t encoder;
+			if (base_64_encoder_init(&encoder, input_file, output_file) == ERROR){
+				fprintf(stderr, "%s \n", "Error initiating encoder");
+				return ERROR;
+			}
+			if (base_64_encoder_encode(&encoder) == ERROR)
+				error = true;
+			base_64_encoder_destroy(&encoder);
+			if (error){
+				fprintf(stderr, "%s \n", "Error decoding");
+				return ERROR;
+			}
+			return SUCCESS;
 		}
-		//return SUCCESS;
 	}
-
-
-
-// OTRA OPCION CON UN POQUITO MENOS DE REPETICIÓN DE CÓDIGO
-		// char* input_file;
-		// char* output_file;
-		// bool decode = false;
-		// inf offset = 0;
-		// if (argv[1] == "-d"){
-		// 	decode = true;
-		// 	offset = 1;
-		// }
-		// else if (argv[1 + offset] == "-i"){
-		// 	input_file = argv[2 + offset];
-		// 	output_file = argv[4 + offset];
-		// }
-		// else if (argv[1 + offset] == "-o"){
-		// 	input_file = NULL;
-		// 	output_file = argv[2 + offset];
-		// }else{
-		// 	perror("Invalid command");//HAGO UN STDERR??? asi ta bien??
-		// 	return ERROR;  // o puede haber otra alternativa??
-		// }
-		// if (decode) decode(input_file, output_file); 
-		// else encode(input_file, output_file);
-		// return SUCCESS;
 }
